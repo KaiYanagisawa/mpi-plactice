72081a5a40c339d12466a80023a4a40b3bdce01a
KaiYanagisawa <12730ky@gmail.com>
2024-01-20 14:23:03 +0900

    outputファイル読み込み処理完了

--- mpi_project/Problem1/sw-sample.090316/sw-sort.c
+++ mpi_project/Problem1/sw-sample.090316/sw-sort.c
@@ -7,6 +7,8 @@
 #include <limits.h>
 #include <mpi.h>
 
+#define LINE_MAX 2048
+
 typedef struct
 {
   char query_sequence[128];
@@ -34,8 +36,8 @@ int main(int argc, char **argv)
     int count = 0;
     printf("sort_start\n");
 
-    output_info sequences[200];
-    // sequences = (output_info *)malloc(count);
+    output_info *sequences;
+    sequences = (output_info *)malloc(1 * sizeof(output_info));
 
     FILE *input_file;
     input_file = fopen(file_output, "r");
@@ -44,24 +46,26 @@ int main(int argc, char **argv)
       fprintf(stderr, "fopen failed: input_file\n");
       exit(EXIT_FAILURE);
     }
-    // while (count < 200 && fscanf(input_file,
-    //               "Query sequence: %s\nDatabase sequence: %s\nBest score: %d\nQ: %s\nD: %s\n\n",
-    //               sequences[count].query_sequence,
-    //               sequences[count].database_sequence,
-    //               &sequences[count].bestScore,
-    //               sequences[count].q,
-    //               sequences[count].d) != EOF)
-    // {
-    //   count++;
-    //   // sequences = (output_info *)realloc_e(sequences, count * sizeof(output_info), "sequences");
-    // }
+    while (fgets(line, LINE_MAX, input_file) != NULL)
+    {
+      sscanf(line, "Query sequence: %s", sequences[count].query_sequence);
+      fgets(line, LINE_MAX, input_file);
+      sscanf(line, "Database sequence: %s", sequences[count].database_sequence);
+      fgets(line, LINE_MAX, input_file);
+      sscanf(line, "Best score: %d", &sequences[count].bestScore);
+      fgets(line, LINE_MAX, input_file);
+      sscanf(line, "Q: %s", sequences[count].q);
+      fgets(line, LINE_MAX, input_file);
+      sscanf(line, "D: %s", sequences[count].d);
+      fgets(line, LINE_MAX, input_file);
+
+      count++;
+
+      sequences = (output_info *)realloc(sequences, count * sizeof(output_info));
+    }
     fclose(input_file);
 
-    // printf("end\n");
-    // printf("query = %d\n", sequences[1].bestScore);
-    // qsort(sequences, count)
-
-    // free(sequences);
+    free(sequences);
   }
 
   MPI_Finalize();
