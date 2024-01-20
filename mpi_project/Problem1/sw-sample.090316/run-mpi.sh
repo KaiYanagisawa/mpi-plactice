52fe50b0e2baf31dd952616d4f47a5af211e3fea
KaiYanagisawa <12730ky@gmail.com>
2024-01-20 12:28:30 +0900

    runファイル sortファイルの追加と合計時間計算

--- mpi_project/Problem1/sw-sample.090316/run-mpi.sh
+++ mpi_project/Problem1/sw-sample.090316/run-mpi.sh
@@ -3,6 +3,7 @@
 #
 CORE=${2:-2}
 BIN="mpiexec -n $CORE ./sw-mpi"
+SORT_BIN="mpiexec -n $CORE ./sw-sort"
 TIMECMD="/usr/bin/time -p"
 
 DIR=${1:-p0}
@@ -16,6 +17,20 @@ LOG=$DIR/log.$DATE
 
 echo "# testset $DIR"
 $TIMECMD -o $LOG $BIN $SCOREMATRIX $QUERY $DATABASE $OUTPUT > $OUTPUT
+$TIMECMD -ao $LOG $SORT_BIN $OUTPUT
+TOTAL_REAL=0
+TOTAL_USER=0
+TOTAL_SYS=0
+while read -r line; do
+  case $line in
+    "real "*) TOTAL_REAL=$(echo "$TOTAL_REAL + $(awk '{print $2}' <<< "$line")" | bc -l | sed -e 's/^\./0./g') ;;
+    "user "*) TOTAL_USER=$(echo "$TOTAL_USER + $(awk '{print $2}' <<< "$line")" | bc -l | sed -e 's/^\./0./g') ;;
+    "sys "*) TOTAL_SYS=$(echo "$TOTAL_SYS + $(awk '{print $2}' <<< "$line")" | bc -l | sed -e 's/^\./0./g') ;;
+  esac
+done < $LOG
+echo "real $TOTAL_REAL" > $LOG
+echo "user $TOTAL_USER" >> $LOG
+echo "sys $TOTAL_SYS" >> $LOG
 cat $LOG
 
 P0SAMPLE=p0/output.sample
